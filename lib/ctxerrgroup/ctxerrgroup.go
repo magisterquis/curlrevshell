b35f58a0bf31470826c33a9c5c68e0560b2fda72
Stuart <jsmcmurrayjr@gmail.com>
2025-01-07 13:49:19 +0100

    Embed, don't wrap, errgroup.Group

--- lib/ctxerrgroup/ctxerrgroup.go
+++ lib/ctxerrgroup/ctxerrgroup.go
@@ -6,7 +6,7 @@ package ctxerrgroup
  * Like errgroup, but with more contexts
  * By J. Stuart McMurray
  * Created 20240324
- * Last Modified 20241226
+ * Last Modified 20250105
  */
 
 import (
@@ -20,25 +20,25 @@ import (
 // add goroutines.  Group's undocumented methods directly wrap its embedded
 // errgroup.Group.
 type Group struct {
-	eg *errgroup.Group
+	errgroup.Group
 }
 
 // WithContext returns a new Group, similar to errgroup.WithContext.
 func WithContext(ctx context.Context) (*Group, context.Context) {
 	eg, ectx := errgroup.WithContext(ctx)
-	return &Group{eg: eg}, ectx
+	return &Group{Group: *eg}, ectx
 }
 
 // GoContext is like errgroup.Group.Go, but passes ctx to the called function.
 // The passed-in context is usually the context returned from WithContext.
 func (g *Group) GoContext(ctx context.Context, f func(context.Context) error) {
-	g.eg.Go(func() error { return f(ctx) })
+	g.Group.Go(func() error { return f(ctx) })
 }
 
 // GoTag is like GoContext, but errors returned by f will be wrapped in a
 // TaggedError..
 func (g *Group) GoTag(ctx context.Context, tag string, f func(context.Context) error) {
-	g.eg.Go(func() error {
+	g.Group.Go(func() error {
 		err := f(ctx)
 		if nil != err {
 			err = TaggedError{Tag: tag, Err: err}
@@ -47,10 +47,6 @@ func (g *Group) GoTag(ctx context.Context, tag string, f func(context.Context) e
 	})
 }
 
-func (g *Group) Go(f func() error)         { g.eg.Go(f) }
-func (g *Group) SetLimit(n int)            { g.eg.SetLimit(n) }
-func (g *Group) TryGo(f func() error) bool { return g.eg.TryGo(f) }
-func (g *Group) Wait() error               { return g.eg.Wait() }
 
 // TaggedError is the error type returned by [Group.GoTag].
 type TaggedError struct {
--- lib/ctxerrgroup/ctxerrgroup_test.go
+++ lib/ctxerrgroup/ctxerrgroup_test.go
@@ -5,7 +5,7 @@ package ctxerrgroup
  * Tests for ctxerrgroup.go
  * By J. Stuart McMurray
  * Created 20241226
- * Last Modified 20241226
+ * Last Modified 20250105
  */
 
 import (
@@ -14,6 +14,61 @@ import (
 	"errors"
 	"testing"
 )
+
+// Make sure it works if we don't use WithConetxt.
+func TestGroup_NoContext(t *testing.T) {
+	/* ...if someone returns an error. */
+	t.Run("with_error", func(t *testing.T) {
+		eg := new(Group)
+		testErr := errors.New("test error")
+		eg.Go(func() error { return nil })
+		eg.Go(func() error { return testErr })
+		if got := eg.Wait(); nil == got {
+			t.Errorf("Got nil error")
+		} else if !errors.Is(got, testErr) {
+			t.Errorf(
+				"Incorrect error:\n got: %s\nwant: %s",
+				got,
+				testErr,
+			)
+		}
+
+	})
+	/* ...if no one returns an error. */
+	t.Run("no_error", func(t *testing.T) {
+		eg := new(Group)
+		var (
+			okGo, okGoContext, okGoTag bool
+		)
+		eg.Go(func() error { okGo = true; return nil })
+		eg.GoContext(
+			context.Background(),
+			func(_ context.Context) error {
+				okGoContext = true
+				return nil
+			},
+		)
+		eg.GoTag(
+			context.Background(),
+			"kittens",
+			func(_ context.Context) error {
+				okGoTag = true
+				return nil
+			},
+		)
+		if err := eg.Wait(); nil != err {
+			t.Fatalf("Wait error: %s", err)
+		}
+		check := func(n string, b bool) {
+			if !b {
+				t.Errorf("%s not set", n)
+			}
+		}
+		check("okGo", okGo)
+		check("okGoContext", okGoContext)
+		check("okGoTag", okGoTag)
+	})
+}
 
 func TestGroupGoTag(t *testing.T) {
 	tag := "kittens"
