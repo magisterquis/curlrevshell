f46e53e5d83b106c2d071936a7f850b1fe1fd596
Stuart <jsmcmurrayjr@gmail.com>
2026-08-13 06:08:15 +0200

    Build tools in less-regularly-named directories

--- src/mk/tools.mk
+++ src/mk/tools.mk
@@ -2,21 +2,22 @@
 # Build other tools
 # By J. Stuart McMurray
 # Created 20251209
-# Last Modified 20251209
+# Last Modified 20260812
 
 TOOLSDIR  = tools
-TOOLS    != find\
-	./lib/*/cmd\
-	-maxdepth 1\
-	-mindepth 1\
-	-type d
-TOOLS    := ${TOOLS:T:T:S,^,${TOOLSDIR}/,}
+CMDDIRS  != find . -name cmd -type d
+TOOLDIRS != find ${CMDDIRS} -type d -mindepth 1 -maxdepth 1
+TOOLS     =
+
+# Build ALL the tools.
+.for TOOLDIR in ${TOOLDIRS}
+TOOL         = ${TOOLSDIR}/${TOOLDIR:T}
+TOOLS       += ${TOOLSDIR}/${TOOLDIR:T}
+${TOOL}_DIR  = ${TOOLDIR}
+${TOOL}! .NOTMAIN
+	go build ${GOBUILDFLAGS} -o $@ ${${@}_DIR}
+.endfor
+# On second thought, this was perhaps a bit unkind.
 
 tools: ${TOOLS} .NOTMAIN ## Build supporting tools
 .PHONY: tools
-
-# Build ALL the tools.
-.for TOOL in ${TOOLS}
-${TOOL}! .NOTMAIN
-	go build ${GOBUILDFLAGS} -o $@ ./lib/${@F}/cmd/${@F}
-.endfor
